7e0ccf63165f061ecee575cd80a3beb4f33a0d4b
Daniil Asipenka <ocipenkoden@gmail.com>
2021-02-15 00:31:42 +0300

    feat: secretary-cabinet

---
+++ src/app/components/cabinet/secretary/secretary-cabinet/secretary-cabinet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secretary-cabinet',
+  templateUrl: './secretary-cabinet.component.html',
+  styleUrls: ['./secretary-cabinet.component.scss']
+})
+export class SecretaryCabinetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
